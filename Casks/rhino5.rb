20f8661949d7a32239db3a353bbb28d7ae242989
Kye Shimizu <kyesh@Kyes-MacBook-Pro.local>
2020-12-24 19:12:35 +0900

    initial commit

---
+++ Casks/rhino5.rb
@@ -0,0 +1,20 @@
+cask 'rhino5' do
+    version '5.5.5'
+    sha256 'b6669fd629fd1f339cd03cebf48dcb9f6d5604245aa12df5e805407b6e9698d9'
+  
+    # mcneel.com was verified as official when first introduced to the cask
+    url "https://files.mcneel.com/Releases/Rhino/#{version.major}.0/Mac/Rhinoceros_#{version}.dmg"
+    appcast "https://files.mcneel.com/rhino/#{version.major}.0/mac/#{version.major}CcommercialUpdates.xml"
+    name 'Rhinoceros'
+    homepage 'https://www.rhino3d.com/'
+  
+    auto_updates true
+  
+    app 'Rhinoceros.app'
+  
+    zap trash: [
+                 '~/Library/Preferences/com.mcneel.rhinoceros.plist',
+                 '~/Library/Caches/com.mcneel.rhinoceros',
+                 '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.mcneel.rhinoceros.sfl*',
+               ]
+  end
