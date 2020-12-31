f2906ed9bc074b0d25b20813e1bae34e390c59b6
kkshmz <kyeshimizu@gmail.com>
2020-12-31 09:39:22 +0900

    typo

--- Casks/dume.rb
+++ Casks/dume.rb
@@ -2,7 +2,7 @@ cask "dume" do
     version "0.1.5"
     sha256 "b4061d3a44bc0faf804e8749247b19143252b248555492c66f637bb7418c70b7"
   
-    url "https://github.com/Rainbox-dev/DuME/releases/download/v#{version}/DuME_#{version}.dmg",
+    url "https://github.com/Rainbox-dev/DuME/releases/download/v#{version}/DuME-#{version}.dmg",
       verified: "github.com/Rainbox-dev/DuME"
     appcast "https://github.com/Rainbox-dev/DuME/releases.atom"
     name "DuME"
