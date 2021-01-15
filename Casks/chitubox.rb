00c517ac64e453fdf65efaf69deb2a4a0793cb13
kkshmz <kyeshimizu@gmail.com>
2021-01-15 14:04:12 +0900

    add chitubox

---
+++ Casks/chitubox.rb
@@ -0,0 +1,11 @@
+cask "chitubox-free" do
+  version "1.7.0"
+  sha256 "40938d0653da9fa90aa9f90cc3cc0dd094d756ec2b5c3b9d1548db68f3b81a63"
+
+  url "https://sac.chitubox.com/software/download.do?softwareId=17839&softwareVersionId=v#{version}&fileName=CHITUBOX_V#{version}.dmg"
+  name "Chitubox Free"
+  desc "All in one SLA/ DLP/ LCD Slicer"
+  homepage ""
+
+  app ""
+end
