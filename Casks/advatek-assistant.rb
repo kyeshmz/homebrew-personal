03452298bd704d67cd3fc0f68a546bc3fda86a06
kkshmz <kyeshimizu@gmail.com>
2021-05-25 10:49:41 +0900

    add advatek

---
+++ Casks/advatek-assistant.rb
@@ -0,0 +1,12 @@
+cask "advatek-assistant"
+do
+    version "2.3.24"
+    sha256 "caffef1041468b1a1ca238a7937754d03473572cc2d613bb8b66d02cf0ff5a34"
+  
+    url "https://www.advateklights.com/downloads/advatek-assistant/osx/V#{version}"
+    name "advatek-assistant"
+    desc "The Advatek Assistant is a computer application that allows you to easily configure your Advatek control devices."
+    homepage "https://www.advateklights.com/"
+  
+    app ""
+end
--- Casks/chitubox.rb
+++ Casks/chitubox.rb
@@ -1,5 +1,5 @@
 cask "chitubox-free" do
-  version "1.7.0"
+  version "1.8.1"
   sha256 "40938d0653da9fa90aa9f90cc3cc0dd094d756ec2b5c3b9d1548db68f3b81a63"
 
   url "https://sac.chitubox.com/software/download.do?softwareId=17839&softwareVersionId=v#{version}&fileName=CHITUBOX_V#{version}.dmg"
