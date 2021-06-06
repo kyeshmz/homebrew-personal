35867ddafeda1c7a8e903a0d4abc7a799d7a9f16
kyeshmz <kyeshimizu@gmail.com>
2021-06-06 14:15:20 +0900

    add homebrew doctor changes

--- Casks/advatek-assistant.rb
+++ Casks/advatek-assistant.rb
@@ -1,5 +1,4 @@
-cask "advatek-assistant"
-do
+cask "advatek-assistant" do
     version "2.3.24"
     sha256 "caffef1041468b1a1ca238a7937754d03473572cc2d613bb8b66d02cf0ff5a34"
   
@@ -7,6 +6,5 @@ do
     name "advatek-assistant"
     desc "The Advatek Assistant is a computer application that allows you to easily configure your Advatek control devices."
     homepage "https://www.advateklights.com/"
-  
-    app ""
+    app "Advatek Assistant"
 end
