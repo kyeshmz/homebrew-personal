5151ae047c636b4fdeccc2ab5638f2f65a73bc12
kye shimizu <kyeshimizu@gmail.com>
2022-03-01 16:45:19 +0900

    update

--- Casks/dume.rb
+++ Casks/dume.rb
@@ -1,6 +1,6 @@
 cask "dume" do
-    version "0.1.5"
-    sha256 "5d6d1919987a7f7c158f149e62ad53a8a68cdb88596abb851ec73657884f31c6"
+    version "0.3.0"
+    sha256 "b91428cb0afd0647c38b0fd9ffdf2619511f17ad37c463e1417bfe82408a9aba"
   
     url "https://github.com/Rainbox-dev/DuME/releases/download/v#{version}/DuME-#{version}.dmg",
       verified: "github.com/Rainbox-dev/DuME"
