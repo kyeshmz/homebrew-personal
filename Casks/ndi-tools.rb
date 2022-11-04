02d1881d47d67826960b578567f84b9832639a35
kye shimizu <kyeshimizu@gmail.com>
2022-11-04 10:35:40 +0900

    add nditools

---
+++ Casks/ndi-tools.rb
@@ -0,0 +1,12 @@
+cask "ndi-tools" do
+    version "5.5.2.15"
+    sha256 "b2c606848ac3936886bd0345ce0dec9f60b460fa1d56341482aa8aba7002dbd9"
+  
+    url "https://go.ndi.tv/tools-for-mac"
+    name "ndi-tools"
+    desc "Ndi tools for mac"
+    homepage "https://www.ndi.tv"
+  
+    pkg "NDIToolsInstaller.pkg"
+  end
+  
